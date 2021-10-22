f20f15f162107f8ee98e74a20f9b7224e17a76f3
sadyrbek <gggggewgw@gmail.com>
2021-10-22 08:55:15 +0600

    'first'

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items:1,
+     loop:true,
+        autoplay:true,
+        autoplayTimeout: 1500,
+        smartSpeed: 250,
+animateOut: true,
+autoplayHoverPause: true,
+    });
+});
+
